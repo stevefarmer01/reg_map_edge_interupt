3726e6b2c1c158d647044a48cbb18508ec89e867
Steve Farmer <stephenrfarmer@hotmail.com>
2016-02-04 23:35:38 +0000

    First commit

---
+++ source/reg_map_edge_interupt.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04.02.2016 23:33:56
+-- Design Name: 
+-- Module Name: reg_map_edge_interupt - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity reg_map_edge_interupt is
+    Port ( clk : in STD_LOGIC);
+end reg_map_edge_interupt;
+
+architecture Behavioral of reg_map_edge_interupt is
+
+begin
+
+
+end Behavioral;
